e7a4c17b375cd2063239cef91b5d462f1a788918
Raashid Ansari <raashid.ansari@silabs.com>
2026-04-05 02:34:37 -0400

    remove stuff that shouldn't be committed ever

--- scripts/rpi/ingestion_service.py
+++ scripts/rpi/ingestion_service.py
@@ -5,15 +5,6 @@ import os
 # ========================
 # Update these with your own Databricks/ZeroBus credentials
 
-# ZeroBus Ingestion Configuration
-DATABRICKS_VOLUME_PATH = "/Volumes/mlops_dev/default/audio_raw"
-ZEROBUS_SERVER_ENDPOINT = "7405615984054316.zerobus.southcentralus.azuredatabricks.net"
-ZEROBUS_WORKSPACE_URL = "https://adb-7405615984054316.16.azuredatabricks.net"
-ZEROBUS_TABLE_NAME = "mlops_dev.default.stream_audio_metadata"
-ZEROBUS_CLIENT_ID = "2cf06ebd-b4ad-40f8-91be-74e576e147f5"
-ZEROBUS_CLIENT_SECRET = "dose3ecfb65d12bdc5a1a8d228e140e34883"
-
-
 os.environ["DATABRICKS_VOLUME_PATH"] = DATABRICKS_VOLUME_PATH
 os.environ["ZEROBUS_SERVER_ENDPOINT"] = ZEROBUS_SERVER_ENDPOINT
 os.environ["ZEROBUS_WORKSPACE_URL"] = ZEROBUS_WORKSPACE_URL
